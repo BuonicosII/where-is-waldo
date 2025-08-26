0561b1750d3a63aeb2d3626d36dfed3bc2490133
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2025-08-26 20:45:45 +0200

    Add buttons to finish game screen

--- src/components/bestScores/BestScores.jsx
+++ src/components/bestScores/BestScores.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import style from "./BestScores.module.css";
 
 export default function BestScores() {
@@ -7,11 +8,35 @@ export default function BestScores() {
     { id: 1, user: "AAA", length: 75 },
     { id: 2, user: "BBB", length: 90 },
   ];
+
+  const navigate = useNavigate();
+
   const [name, setName] = useState(null);
+  const [open, setOpen] = useState(false);
 
   function inputUpdate(e) {
     e.preventDefault();
     setName(document.querySelector("#name").value);
+  }
+
+  function returnStart() {
+    if (game.length) {
+      //cancella localStorage
+    }
+
+    navigate("/");
+  }
+
+  function saveRecord(e) {
+    e.preventDefault();
+
+    if (topGames.length < 10 || topGames[9].length > game.length) {
+      //aggiorna il record
+
+      //cancella localStorage
+
+      setOpen(false);
+    }
   }
 
   return (
@@ -42,32 +67,51 @@ export default function BestScores() {
             </span>
           </div>
         )}
-        {game.length && (
-          <div id={style.saveGameHolder}>
-            <form id="savegame">
-              <div className={style.inputHolder}>
-                <label htmlFor="">Enter a name:</label>
-                <input
-                  onChange={inputUpdate}
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={name}
-                  required
-                />
-              </div>
-            </form>
-            <div className={style.buttonHolder}>
-              <button type="submit" form="newgame">
+        <div className={style.buttonHolder}>
+          <button onClick={returnStart}>Go to new game</button>
+          {game.length &&
+            (topGames.length < 10 || topGames[9].length > game.length) && (
+              <button
+                onClick={() => {
+                  setOpen(true);
+                }}
+              >
                 Save record
               </button>
+            )}
+        </div>
+        {open && (
+          <div className={style.popupHolder}>
+            <div id={style.saveGameHolder}>
+              <form id="savegame" onSubmit={saveRecord}>
+                <div className={style.inputHolder}>
+                  <label htmlFor="">Enter a name:</label>
+                  <input
+                    onChange={inputUpdate}
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    required
+                  />
+                </div>
+              </form>
+              <div className={style.buttonHolder}>
+                <button type="submit" form="newgame">
+                  Save record
+                </button>
+                <button
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
         )}
       </div>
     </main>
-    /*
-
-    */
   );
 }
